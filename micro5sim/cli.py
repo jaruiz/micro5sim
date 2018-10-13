f8f8af21724519e27308b561f33b11b0e48715af
Jose A. Ruiz <ja_rd@outlook.com>
2018-10-13 17:34:50 +0100

    Move elf reading code into the MCU class for clarity.

--- micro5sim/cli.py
+++ micro5sim/cli.py
@@ -4,100 +4,7 @@ import argparse
 import struct
 import sys
 
-from elftools.elf.elffile import ELFFile
-from elftools.elf.constants import SH_FLAGS
-
 import m5soc
-import m5cpu
-
-DEFAULT_ROM_ADDR =  m5soc.DEFAULT_ROM_ADDR
-DEFAULT_ROM_WORDS = 256*1024
-DEFAULT_RAM_ADDR =  m5soc.DEFAULT_RAM_ADDR
-DEFAULT_RAM_WORDS = 64*1024
-
-
-def _read_elf(filename, rom, ram, opts):
-    """ Read all sections into the memory area they're contained in.
-        Ignore any section that is fully outside all memory areas but 
-        fail if any section is part in part out.
-        Assume no section straddles two memory areas and areas don't overlap.
-        Assume data in all sections is little endian.
-
-        Return true if any instructions were loaded at the reset address.
-    """
-    try:
-        fi = open(filename)
-        elf = ELFFile(fi)
-
-        # Display some info in a format resembling riscvOVPsim's.
-        print "Read object file '%s'" % filename
-        print "Sections loaded:"
-        print "  Area          Section           Address     MemSize"
-
-
-        executable_stuff_at_reset_addr = False
-        for section in elf.iter_sections():
-            addr = section.header['sh_addr']
-            size = section.header['sh_size']
-            flags = section.header['sh_flags']
-
-            # Ignore any sections not meant to be loaded to memory.
-            if not (flags & SH_FLAGS.SHF_ALLOC): continue
-
-            # Remember if we load instructions on the reset address.
-            if (flags & SH_FLAGS.SHF_EXECINSTR):            
-                if m5cpu.ADDR_RESET >= addr and m5cpu.ADDR_RESET < (addr + size):
-                    executable_stuff_at_reset_addr = True
-
-            # Find out which area contains this section.
-            if addr >= opts.rom_addr and (addr+size) < (opts.rom_addr + opts.rom_size):
-                # Section is within ROM area.
-                _print_section_info(section, "ROM")
-                
-                # Fail if a writeable section wants to live in ROM area.
-                # FIXME pass option on to mcu object!
-                if not opts.rom_writeable and (flags & SH_FLAGS.SHF_WRITE):
-                    print "Error: Section '%s' in ROM area is writeable" % (section.name)
-                    sys.exit(5)
-
-                # Otherwise just copy the little endian stuff
-                _load_section_data(section, addr - opts.ram_addr, rom)
-                
-
-            elif addr >= opts.ram_addr and (addr+size) < (opts.ram_addr + opts.ram_size):
-                # Section is within RAM area.
-                _print_section_info(section, "RAM")
-                # Just copy the section to RAM. Even if it is read-only.
-                _load_section_data(section, addr - opts.rom_addr, ram)
-                
-        
-            else:
-                # Not fully contained by ROM or RAM areas.
-                # If there's any overlap then fail, otherwise ignore section.
-                pass
-                # FIXME fail on overlap
-
-        fi.close()
-
-    except IOError as e:
-        print >> sys.stderr, "Error reading elf file:"
-        print >> sys.stderr, str(e)
-        sys.exit(2)
-
-    return executable_stuff_at_reset_addr
-
-
-def _print_section_info(section, area):
-    print "  %-12s  %-16s  0x%08x  0x%08x" % (area, section.name, section.header['sh_addr'], section.header['sh_size'])    
-
-
-def _load_section_data(section, offset, mem):
-    data = bytearray()
-    data.extend(section.data())
-    for j in range(len(data)/4):
-        i = j * 4
-        word = (data[i+0]<<0) | (data[i+1]<<8) | (data[i+2]<<16) | (data[i+3]<<24)
-        mem[j] = word
 
 
 
@@ -116,17 +23,17 @@ def _parse_cmdline():
     parser.add_argument('elf', metavar="ELF-FILE", type=str,
         help="Executable (elf) file")
     parser.add_argument('--rom-addr', metavar="NUM",
-        help="base address of ROM area. Defaults to 0x%08x" % (DEFAULT_ROM_ADDR),
-        type=int, default=DEFAULT_ROM_ADDR)
+        help="base address of ROM area. Defaults to 0x%08x" % (m5soc.DEFAULT_ROM_ADDR),
+        type=int, default=m5soc.DEFAULT_ROM_ADDR)
     parser.add_argument('--rom-size', metavar="NUM",
-        help="size of ROM area in 32-bit words. Defaults to %d KB" % (DEFAULT_ROM_WORDS/256),
-        type=int, default=DEFAULT_ROM_WORDS)
+        help="size of ROM area in 32-bit words. Defaults to %d KB" % (m5soc.DEFAULT_ROM_WORDS/256),
+        type=int, default=m5soc.DEFAULT_ROM_WORDS)
     parser.add_argument('--ram-addr', metavar="NUM",
-        help="base address of RAM area. Defaults to 0x%08x" % (DEFAULT_RAM_ADDR),
-        type=int, default=DEFAULT_ROM_ADDR)
+        help="base address of RAM area. Defaults to 0x%08x" % (m5soc.DEFAULT_RAM_ADDR),
+        type=int, default=m5soc.DEFAULT_ROM_ADDR)
     parser.add_argument('--ram-size', metavar="NUM",
-        help="size of RAM area in 32-bit words. Defaults to %d KB" % (DEFAULT_RAM_WORDS/256),
-        type=int, default=DEFAULT_RAM_WORDS)
+        help="size of RAM area in 32-bit words. Defaults to %d KB" % (m5soc.DEFAULT_RAM_WORDS/256),
+        type=int, default=m5soc.DEFAULT_RAM_WORDS)
     parser.add_argument('--num-inst', metavar="NUM",
         help="maximum number of instructions to execute. Defaults to unlimited",
         type=int, default=None)
@@ -143,14 +50,17 @@ def main():
     """Entry point when installed as package."""
     opts = _parse_cmdline()
     
+
     rom = [0] * opts.rom_size
     ram = [0] * opts.ram_size
 
-    if not _read_elf(opts.elf, rom, ram, opts):
+    soc = m5soc.SoC(rom, ram)
+
+    soc.rom_writeable = opts.rom_writeable
+
+    if not soc.read_elf(opts.elf):
         print >> sys.stderr, "No executable instructions at reset address."
         sys.exit(4)
-
-    soc = m5soc.SoC(rom, ram)
 
     soc.delta_log_file = open("log.txt", "w")
     soc.asm_log_file = open("trace.txt", "w")
--- micro5sim/m5soc.py
+++ micro5sim/m5soc.py
@@ -3,12 +3,16 @@
 import argparse
 import sys
 
+from elftools.elf.elffile import ELFFile
+from elftools.elf.constants import SH_FLAGS
+
 import m5cpu
 
 
 DEFAULT_ROM_ADDR =  0x00000000
+DEFAULT_ROM_WORDS = 256*1024
 DEFAULT_RAM_ADDR =  0x40000000
-
+DEFAULT_RAM_WORDS = 64*1024
 
 
 class SoC(object):
@@ -23,6 +27,7 @@ class SoC(object):
         self.cpu = m5cpu.CPU(self._load, self._store, self._log_delta, self._log_asm)
         self.delta_log_file = None
         self.asm_log_file = None
+        self.rom_writeable = False
 
     def reset(self):
         self.cpu.reset()
@@ -30,6 +35,77 @@ class SoC(object):
 
     def run(self, num=None):
         self.cpu.run(num)
+
+
+    def read_elf(self, filename):
+        """ Read all sections into the memory area they're contained in.
+            Ignore any section that is fully outside all memory areas but 
+            fail if any section is part in part out.
+            Assume no section straddles two memory areas and areas don't overlap.
+            Assume data in all sections is little endian.
+
+            Return true if any instructions were loaded at the reset address.
+        """
+        try:
+            fi = open(filename)
+            elf = ELFFile(fi)
+
+            # Display some info in a format resembling riscvOVPsim's.
+            print "Read object file '%s'" % filename
+            print "Sections loaded:"
+            print "  Area          Section           Address     MemSize"
+
+
+            executable_stuff_at_reset_addr = False
+            for section in elf.iter_sections():
+                addr = section.header['sh_addr']
+                size = section.header['sh_size']
+                flags = section.header['sh_flags']
+
+                # Ignore any sections not meant to be loaded to memory.
+                if not (flags & SH_FLAGS.SHF_ALLOC): continue
+
+                # Remember if we load instructions on the reset address.
+                if (flags & SH_FLAGS.SHF_EXECINSTR):            
+                    if m5cpu.ADDR_RESET >= addr and m5cpu.ADDR_RESET < (addr + size):
+                        executable_stuff_at_reset_addr = True
+
+                # Find out which area contains this section.
+                if addr >= self.rom_bot and (addr+size) < self.rom_top:
+                    # Section is within ROM area.
+                    self._print_section_info(section, "ROM")
+                    
+                    # Fail if a writeable section wants to live in ROM area.
+                    if not self.rom_writeable and (flags & SH_FLAGS.SHF_WRITE):
+                        print "Error: Section '%s' in ROM area is writeable" % (section.name)
+                        sys.exit(5)
+
+                    # Otherwise just copy the little endian stuff
+                    self._load_section_data(section, addr - self.rom_bot, self.rom)
+                    
+
+                elif addr >= self.ram_bot and (addr+size) < self.ram_top:
+                    # Section is within RAM area.
+                    self._print_section_info(section, "RAM")
+                    # Just copy the section to RAM. Even if it is read-only.
+                    self._load_section_data(section, addr - self.ram_bot, self.ram)
+                    
+            
+                else:
+                    # Not fully contained by ROM or RAM areas.
+                    # If there's any overlap then fail, otherwise ignore section.
+                    pass
+                    # FIXME fail on overlap
+
+            fi.close()
+
+        except IOError as e:
+            print >> sys.stderr, "Error reading elf file:"
+            print >> sys.stderr, str(e)
+            sys.exit(2)
+
+        return executable_stuff_at_reset_addr
+
 
     def _load(self, addr):
         windex = addr / 4
@@ -50,7 +126,12 @@ class SoC(object):
         if self.rom_bot <= windex < self.rom_top:
             word = self.rom[windex - self.rom_bot]
             word = (word & ~mask) | value
-            self.rom[windex - self.rom_bot] = word
+            if self.rom_writeable:
+                self.rom[windex - self.rom_bot] = word
+            else:
+                # Storing to read-only ROM area. Ignore.
+                print "[0x%08x] warning: writing to read-only address 0x%08x" % (self.PC, addr)
+
         elif self.rom_bot <= windex < self.rom_top:
             word = self.ram[windex - self.ram_bot]
             word = (word & ~mask) | value
@@ -65,6 +146,21 @@ class SoC(object):
         if self.delta_log_file:
             print >> self.delta_log_file, "%08x: r%02d=%08x" % (pc, index, value)
 
+
     def _log_asm(self, asm):
         if self.asm_log_file:
             print >> self.asm_log_file, asm
+
+
+    def _print_section_info(self, section, area):
+        print "  %-12s  %-16s  0x%08x  0x%08x" % (area, section.name, section.header['sh_addr'], section.header['sh_size'])    
+
+
+    def _load_section_data(self, section, offset, mem):
+        data = bytearray()
+        data.extend(section.data())
+        for j in range(len(data)/4):
+            i = j * 4
+            word = (data[i+0]<<0) | (data[i+1]<<8) | (data[i+2]<<16) | (data[i+3]<<24)
+            mem[j] = word
+
